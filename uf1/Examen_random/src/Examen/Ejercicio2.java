2e386192bcdfce033d5ccd8289a15768ad4b646e
David Artero <d.arterom@gmail.com>
2017-10-26 16:20:50 +0200

    Examenes random: ejercicios

--- uf1/Examen_random/src/Examen/Ejercicio2.java
+++ uf1/Examen_random/src/Examen/Ejercicio2.java
@@ -1,10 +1,40 @@
 package Examen;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		int cant = 0, num, i=0, cont=0;
+		
+		do{
+			System.out.println("Introduce la cantidad de numeros");
+			if(s.hasNextInt()){
+				cant=s.nextInt();
+				i++;
+			}
+		}while(i<1);
+		
+		i=0;
+		
+		while(i<cant){
+			System.out.println("introduce el numero: "+i);
+			
+			if(s.hasNextInt()){
+				num=s.nextInt();
+				if(num<0){
+					System.out.println("num: "+num);
+					cont++;
+				}
+				i++;
+			}else{
+				System.out.println("Error no es una valor entero.");
+				s.next();
+			}
+		}
+		System.out.println("\nla cantidad de numeros negativos es: "+cont);
 	}
 
 }
--- uf1/Examen_random/src/Examen/Ejercicio4.java
+++ uf1/Examen_random/src/Examen/Ejercicio4.java
@@ -1,10 +1,37 @@
 package Examen;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		int num = 0, i=0, cont=0;
+		
+		while(i<1){
+			System.out.println("Introduce un numero < 10");
+			if(s.hasNextInt()){
+				num=s.nextInt();
+				if(num>=10){
+					i++;
+				}else{
+					System.out.println("El valor: "+num+"< 10");
+				}
+			}else{
+				System.out.println("El valor no es entero");
+			}
+			
+		}
+		System.out.println("El num es: "+num);
+		i=0;
+		while(i<num){
+			num=num/10;
+			System.out.println(num);
+				cont++;
+				i++;
+		}
+		System.out.println("La cantida es: "+cont);
 	}
 
 }
